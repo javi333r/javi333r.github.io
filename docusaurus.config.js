5ab7d4ed0e6634ea92c7db46ac49691150aecddc
Javier Bravo <113664960+javi333r@users.noreply.github.com>
2025-09-26 13:00:00 +0200

    SVG guapo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'Infra Synthesis',
   tagline: 'Kubernetes • GitHub Actions • Infrastructure as Code',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.svg',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
@@ -72,7 +72,7 @@ const config = {
         title: 'Infra Synthesis',
         logo: {
           alt: 'Infra Synthesis Logo',
-          src: 'img/logo.svg',
+          src: 'img/infra-synthesis-logo.svg',
         },
         items: [
           {
